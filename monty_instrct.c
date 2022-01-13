cf4b97f6dad437686e6679aea0904f5dfed6fee7
Camaltra <mickael.boillaud@gmail.com>
2022-01-13 01:36:53 +0000

    check something

--- monty_instrct.c
+++ monty_instrct.c
@@ -69,7 +69,7 @@ void pint(stack_t **stack, unsigned int line_number)
 		free(globalVar.lineBuff);
 		freeAll();
 		fclose(globalVar.file);
-		exit(0);
+		exit(EXIT_FAILURE);
 	}
 
 	printf("%d\n", (*stack)->n);
